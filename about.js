d2b23e26100b870bb31de1238a51731ef1d5f074
mrrogersweborhood <61599175+mrrogersweborhood@users.noreply.github.com>
2025-10-15 21:19:20 -0500

    Delete about.js

--- about.js
+++
@@ -1,9 +0,0 @@
-// about.js v2.65
-export default async function renderAbout(root) {
-  root.innerHTML = `
-    <section class="container" style="max-width:800px;margin:2rem auto;">
-      <h1 class="page-title">About</h1>
-      <p>The Oklahoma Observer — Comforting the afflicted and afflicting the comfortable since 1969.</p>
-    </section>
-  `;
-}
