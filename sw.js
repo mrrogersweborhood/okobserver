35c187c799e6bff2f4e4cd0fbcff68c850c578b3
mrrogersweborhood <61599175+mrrogersweborhood@users.noreply.github.com>
2025-11-21 15:59:48 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - HTML (navigation): network-first, offline fallback to cached index.
    - Static assets (CSS/JS/images): cache-first with network fill.
 */
-const SW_BUILD   = '2025-11-21R5-hamburgerInit';
+const SW_BUILD   = '2025-11-21R6-searchAppend';
 const CACHE_NAME = 'okobserver-cache-' + SW_BUILD;
 
 // Explicit precache list
